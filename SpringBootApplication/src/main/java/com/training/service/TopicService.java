bfb6a61d97b532068bbed09f01dc3c534609f6cf
pparam <pparamita@sapient.com>
2018-12-13 16:34:08 +0530

    Changed to boot 2.x

--- SpringBootApplication/src/main/java/com/training/service/TopicService.java
+++ SpringBootApplication/src/main/java/com/training/service/TopicService.java
@@ -3,6 +3,7 @@ package com.training.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,22 +13,29 @@ import com.training.repository.TopicRepository;
 
 @Service
 public class TopicService {
-	
+
 	@Autowired
 	private TopicRepository topicRepository;
 
-	/*List<Topic> topicList = new ArrayList(Arrays.asList(new Topic("JV", "Java", "Complete Java Tutorial"),
-			new Topic("JS", "Javascript", "Javascript tutorial"),
-			new Topic("Hadoop", "Big data technologie", "Hadoop Framework")));
-*/
+	/*
+	 * List<Topic> topicList = new ArrayList(Arrays.asList(new Topic("JV",
+	 * "Java", "Complete Java Tutorial"), new Topic("JS", "Javascript",
+	 * "Javascript tutorial"), new Topic("Hadoop", "Big data technologie",
+	 * "Hadoop Framework")));
+	 */
 	public List<Topic> getTopics() {
 		List<Topic> topicList = new ArrayList<Topic>();
-		topicRepository.findAll().forEach(topicList::add);  //findAll() returns an Iterable which has to be converted to a list
+		topicRepository.findAll().forEach(topicList::add); // findAll() returns
+															// an Iterable which
+															// has to be
+															// converted to a
+															// list
 		return topicList;
 	}
 
 	public Topic getTopic(String topicId) {
-		return topicRepository.findOne(topicId);
+		Optional<Topic> optionalTopic = topicRepository.findById(topicId);
+		return optionalTopic.get();
 	}
 
 	public void createTopic(Topic topic) {
@@ -36,27 +44,25 @@ public class TopicService {
 
 	public void updateTopic(String topicId, Topic updatedTopic) {
 
-		/*ListIterator<Topic> iterator = topicList.listIterator();
-		while (iterator.hasNext()) {
-			Topic topic = iterator.next();
-			if (topic.getId().equals(topicId)) {
-				iterator.set(updatedTopic);
-			}
-		}*/
-		topicRepository.save(updatedTopic);  // this sffice for update operation as long as topic instance consists of id 
-		
+		/*
+		 * ListIterator<Topic> iterator = topicList.listIterator(); while
+		 * (iterator.hasNext()) { Topic topic = iterator.next(); if
+		 * (topic.getId().equals(topicId)) { iterator.set(updatedTopic); } }
+		 */
+		topicRepository.save(updatedTopic); // this suffice for update operation
+											// as long as topic instance
+											// consists of id
+
 	}
 
 	public void deleteTopic(String topicId) {
-		/*ListIterator<Topic> iterator = topicList.listIterator();
-		while (iterator.hasNext()) {
-			Topic topic = iterator.next();
-			if (topic.getId().equals(topicId)) {
-				iterator.remove();
-			}
-		}*/
-		
-		topicRepository.delete(topicId);
+		/*
+		 * ListIterator<Topic> iterator = topicList.listIterator(); while
+		 * (iterator.hasNext()) { Topic topic = iterator.next(); if
+		 * (topic.getId().equals(topicId)) { iterator.remove(); } }
+		 */
+
+		topicRepository.deleteById(topicId);
 	}
 
 }
